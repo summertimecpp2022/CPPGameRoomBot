20a7db49a2f2a8446c437cb3eab5233adc59a7c3
Anthony Silva <tenseduke@gmail.com>
2022-06-21 22:28:53 -0700

    Things I have done: Installed necessary packages for PyMongo Created the connection with the bot and database
    To do next:
    Figuring out how to store user data from commands and storing on the database

--- CPPGameRoomBot.py
+++ CPPGameRoomBot.py
@@ -8,11 +8,12 @@ from pymongo import MongoClient
 load_dotenv()       # .env is used to hide our bots token
 
 cluster = MongoClient('mongodb+srv://ASilva98:Grec1998@cppgameroom.h5j3d.mongodb.net/test')     # Connection to database
-db = cluster['userData']
+db = cluster['userData']        # The name of our database
 collection = db['userData']
 
-bot = commands.Bot(command_prefix = "/")
+print('Current collections in database: ' + str(db.list_collection_names()))        # Print current collections in database
 
+bot = commands.Bot(command_prefix = "/")
 
 @bot.event       # Events are how our bot reacts with the server
 async def on_ready():       # Displays a message in the terminal when the bot connects to our Discord server
@@ -26,9 +27,17 @@ async def on_member_join(member):       # on_member_join handles actions when a 
 
 @bot.command(description = 'Gives a list of available commands')
 async def register(ctx):
-    await ctx.respond('Enter your preferred name!')
-    name = await bot.wait_for('message')
-    await ctx.send(f'Thanks for registering {name}!')
+    myCollection = db.users     # db.users is the collection where user data will be held
+    await ctx.send('Enter your preferred name!')        # Bot's message
+
+    name = await bot.wait_for('message', check = lambda message:        # Bot is waiting for a response.
+        message.author == ctx.author and message.channel == ctx.channel)       # Ensures it is by the same user and in the same channel
+    name = name.content     # Assign the content of the message to 'name'
+    discordTag = str(ctx.message.author)
+    myCollection.insert_one({'discord tag': discordTag,
+                             'preferred name': name})       # In the collection, insert their name
+
+    await ctx.send(f'Thanks for registering {name}!')       # Bot confirms registration by repeating their name
 
 bot.run(os.getenv('TOKEN'))
 
